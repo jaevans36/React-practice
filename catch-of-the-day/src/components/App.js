75032014c13116ff54e5d7f968e326f7bd5d8917
Jay Evans <jaevans36@gmail.com>
2018-03-18 09:41:03 +0000

    Created some new components and created a router to manage pages

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import Header from './Header';
+import Inventory from './Inventory';
+import Order from './Order';
 
 class App extends React.Component {
 	render() {
 		return (
 			<div className="catch-of-the-day">
 				<div className="menu">
-					<Header />
+					<Header tagline="penis-fish" />
 				</div>
-				{/* <Inventory /> */}
-				{/* <Order /> */}
+				<Order />
+				<Inventory />
 			</div>
 		);
 	}
--- catch-of-the-day/src/components/Header.js
+++ catch-of-the-day/src/components/Header.js
@@ -1,12 +1,42 @@
 import React from 'react';
 
+// Stateless functional component
+// const Header = props => {
+// 	return (
+// 		<header className="top">
+// 			<h1>
+// 				Catch
+// 				<span className="ofThe">
+// 					<span className="of">of</span>
+// 					<span className="the">the</span>
+// 				</span>
+// 				Day
+// 			</h1>
+// 			<h3 className="tagline">
+// 				<span>
+// 					{props.tagline}
+// 				</span>
+// 			</h3>
+// 		</header>
+// 	);
+// };
+
 class Header extends React.Component {
 	render() {
 		return (
 			<header className="top">
-				<h1>Catch of the Day</h1>
+				<h1>
+					Catch
+					<span className="ofThe">
+						<span className="of">of</span>
+						<span className="the">the</span>
+					</span>
+					Day
+				</h1>
 				<h3 className="tagline">
-					<span>Fresh Dailys</span>
+					<span>
+						{this.props.tagline}
+					</span>
 				</h3>
 			</header>
 		);
